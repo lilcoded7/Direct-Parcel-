9b403e7d377166c1461576aee35ea9ddae0c8f56
coded <lilcoded7@example.com>
2023-01-20 07:06:05 -0800

    updated databse

--- directparcel/settings.py
+++ directparcel/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'directparcel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE'   : 'django.db.backends.postgresql',
+        'NAME'     : 'railway',
+        'USER'     : 'postgres',
+        'PASSWORD' : 'JsMGAque6uXCBC5kXuGg',
+        'HOST'     : 'containers-us-west-54.railway.app',
+        'PORT'     : '5937',
     }
 }
 
